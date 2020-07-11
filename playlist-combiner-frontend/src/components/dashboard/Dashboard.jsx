57b32a3915633ea231504fdcd6a6d11b470e605d
LGXprod <mattghanno@gmail.com>
2020-07-12 03:02:01 +1000

    Using chart js to add charts of a user's spotify stats

--- playlist-combiner-frontend/src/components/dashboard/Dashboard.jsx
+++ playlist-combiner-frontend/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Cookies from "universal-cookie";
-import { access } from "fs";
+// import { access } from "fs";
+import { Bar as BarChart } from "react-chartjs-2";
 
 const axios = require("axios");
 const queryString = require("querystring");
@@ -33,8 +34,22 @@ class Dashboard extends Component {
             fName: "",
             sName: "",
             username: cookie.get("username"),
-            access_token: access_token
+            access_token: access_token, 
+            data: {
+                labels: ["Acousticness", "Danceability", "Energy", "Happiness"],
+                datasets: [
+                  {
+                    data: [],
+                    backgroundColor: "rgba(192, 108, 132, 0.5)",
+                    borderWidth: "2",
+                    borderColor: "#f67280",
+                    borderRadius: "2"
+                  }
+                ]
+            }
         }
+
+        this.chartReference = React.createRef();
     }
 
     getUserInfo = () => {
@@ -57,6 +72,27 @@ class Dashboard extends Component {
 
     render() {
 
+        const options = {
+            legend: {
+                labels: {
+                    fontColor: "#f8b595"
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true,
+                        fontColor: '#f8b595'
+                    },
+                }],
+              xAxes: [{
+                    ticks: {
+                        fontColor: '#f8b595'
+                    },
+                }]
+            }     
+        }
+
         function isUnlinked() {
             const imgStyle = { 
                 width: "20% ", 
@@ -74,27 +110,26 @@ class Dashboard extends Component {
 
             return (
                 <div style={{ width: "25vw" }}>
-                    <img style={imgStyle} src={ require("../dashboard/spotify-logo.png") }/>
+                    <img alt="" style={imgStyle} src={ require("../dashboard/spotify-logo.png") }/>
                     <a style={{ width: "75% ", display: "inline-block"}}
                     href={ url.href }>Your spotify account is not linked</a>
                 </div>
             );
         }
-        
+
         return (
             <div>
                 <h1>Dashboard</h1>
                 <h3>{ this.state.fName + " " + this.state.sName }</h3>
-                <div>
-                    { !(this.state.unlinked) ? isUnlinked() : null }
-                </div>
+                { !(this.state.unlinked) ? isUnlinked() : null }
+                { this.state.unlinked ? <BarChart ref={this.chartReference} data={this.state.data} options={options} /> : null }
             </div>
         );
 
     }
 
     componentDidMount() {
-        const self = this;
+        console.log(this.chartReference)
 
         this.getUserInfo().then((info) => {
             this.setState({ 
@@ -103,6 +138,7 @@ class Dashboard extends Component {
             });
 
             const access_token = this.state.access_token;
+            const self = this;
 
             if (this.state.unlinked) {
                 axios.get("https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50", {
@@ -110,16 +146,63 @@ class Dashboard extends Component {
                         'Authorization': 'Bearer ' + access_token
                     }
                 }).then(function(res) {
-                    // console.log(res)
-                    for (var song of res.data.items) {
-                        axios.get("https://api.spotify.com/v1/audio-features/" + song.id, {
-                            headers: {
-                                'Authorization': 'Bearer ' + access_token
+
+                    function calculateAvgs () {
+                        return new Promise(async (resolve, reject) => {
+                            let averageOfFeatures = {
+                                acousticness: 0,
+                                danceability: 0,
+                                energy: 0,
+                                loudness: 0,
+                                happiness: 0, 
+                                tempo: 0
                             }
-                        }).then(function(featuresRes) {
-                            console.log(featuresRes)
-                        }).catch(err => console.log(err));
+
+                            async function requestSpotify(reject) {
+                                axios.get("https://api.spotify.com/v1/audio-features/" + song.id, {
+                                    headers: {
+                                        'Authorization': 'Bearer ' + access_token
+                                    }
+                                }).then(function(featuresRes) {
+                                    const data = featuresRes.data;
+                                    averageOfFeatures.acousticness = averageOfFeatures.acousticness + Math.round(100*data.acousticness/50);
+                                    averageOfFeatures.danceability = averageOfFeatures.danceability + Math.round(100*data.danceability/50);
+                                    averageOfFeatures.energy = averageOfFeatures.energy + Math.round(100*data.energy/50);
+                                    averageOfFeatures.loudness = averageOfFeatures.loudness + Math.round(100*data.loudness/50);
+                                    averageOfFeatures.happiness = averageOfFeatures.happiness + Math.round(100*data.valence/50);
+                                    averageOfFeatures.tempo = averageOfFeatures.tempo + Math.round(100*data.tempo/50);
+                                }).catch(err => reject(err));
+                            }
+
+                            for (var song of res.data.items) {
+                                await requestSpotify(reject);
+                            }
+
+                            resolve(averageOfFeatures);
+                        });
                     }
+
+                    calculateAvgs().then((averageOfFeatures) => {
+                        let newChartData = [];
+                        console.log("source", averageOfFeatures)
+                        console.log("x", averageOfFeatures.acousticness)
+                        for (let feature in averageOfFeatures) {
+                            newChartData.push(averageOfFeatures[feature]);
+                        }
+
+                        self.setState({
+                            data: {
+                                datasets: [
+                                    {
+                                        data: newChartData
+                                    }
+                                ]
+                            }
+                        });
+
+                        console.log("x", self.state.data.datasets[0].data)
+                    }).catch(err => console.log(err));
+                   
                 }).catch(err => console.log(err));
             }
         }).catch(err => console.log(err));
